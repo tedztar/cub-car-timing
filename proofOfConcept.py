9a380b495c36e7f8457d511122fd72e195edf5ec
tedztar <tedztar@gmail.com>
2016-03-13 16:27:51 -0400

    Made it more redable
    Changed some things to make the code more redable

--- proofOfConcept.py
+++ proofOfConcept.py
@@ -6,18 +6,22 @@ DEBUG = 1
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
+LED=23
+START/STOP=24
+LANE1=25
+LANE2=22
+
 def dptime ():
     waiting = 0
-    GPIO.setup(23, GPIO.OUT)
-    GPIO.output(23, GPIO.LOW)
-    GPIO.setup(24, GPIO.IN)
-    GPIO.setup(25, GPIO.IN) # lane1
-    GPIO.setup(22, GPIO.IN) # lane2
+    GPIO.setup(LED, GPIO.OUT)
+    GPIO.setup(START/STOP, GPIO.IN)
+    GPIO.setup(LANE1, GPIO.IN) # lane1
+    GPIO.setup(LANE2, GPIO.IN) # lane2
 
     print "\nSTART YOUR ENGINES!"
 
     # wait for start button to be pressed
-    while (GPIO.input(24) == GPIO.LOW):
+    while (GPIO.input(STAR) == GPIO.LOW):
         waiting += 1
 
     print "\nSOMETIMES YOU GOTTA RACE..."
@@ -26,7 +30,7 @@ def dptime ():
     start = time.time()
 
     # turn light on
-    GPIO.output(23, GPIO.HIGH)
+    GPIO.output(LED, GPIO.HIGH)
 
     # debounce the button hack
     time.sleep(1)
@@ -42,16 +46,16 @@ def dptime ():
        time.sleep(0.01)
 
        # stop button pressed
-       if (GPIO.input(24) == GPIO.HIGH):
+       if (GPIO.input(START/STOP) == GPIO.HIGH):
            done = time.time() - start
        else:
            # lane1 finished aka light blocked
-           if (lane1 == 0 and GPIO.input(25) == GPIO.LOW):
+           if (lane1 == 0 and GPIO.input(LANE1) == GPIO.LOW):
                lane1 = time.time() - start
                print "\nLANE1: " , '{0:.4g}'.format(lane1)
 
            # lane2 finished aka light blocked
-           if (lane2 == 0 and GPIO.input(22) == GPIO.LOW):
+           if (lane2 == 0 and GPIO.input(LANE2) == GPIO.LOW):
                lane2 = time.time() - start
                print "\nLANE2: " , '{0:.4g}'.format(lane2)
 
@@ -60,7 +64,7 @@ def dptime ():
                done = time.time() - start
 
     # turn light off
-    GPIO.output(23, GPIO.LOW)
+    GPIO.output(LED, GPIO.LOW)
     print "\nDONE: " , '{0:.4g}'.format(done)
 
     # debounce start/stop button
@@ -69,3 +73,4 @@ def dptime ():
 
 dptime() 
 
+GPIO.cleanup()
